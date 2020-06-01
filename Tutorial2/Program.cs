5d328a6a651a5578e450d9ddb0f1e60cbb659f8e
Emmanuella Fatolu <61954156+oceanuella2020@users.noreply.github.com>
2020-06-01 15:19:51 +0100

    fix test

--- Tutorial2/Program.cs
+++ Tutorial2/Program.cs
@@ -7,6 +7,7 @@ namespace Tutorial2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.Write("testing my git in the dev branch. wanna see if i now know what i am doing on git")
         }
     }
 }
